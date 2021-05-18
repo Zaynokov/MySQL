3f34e35691d2d4da1f37ef543656d507bc957f7f
Zaynokov <vlerii99@mail.ru>
2021-05-18 19:25:53 +0300

    Watch shop

---
+++ Скрипт наполнения таблиц.sql
@@ -0,0 +1,50 @@
+﻿INSERT INTO brands (name, country) 
+VALUES
+	("Tissot", "Швейцария"),
+	("Casio", "Япония"),
+	("Romanson", "Южная Корея"),
+	("Certina", "Швейцария"),
+	("Rado", "Швейцария"),
+	("Disel", "США");
+
+INSERT INTO glass_type_categories (name)
+VALUES 
+	("Сапфировое стекло"),
+	("Минеральное стекло"),
+	("Органическое стекло"),
+	("Хезалитовое стекло");
+
+INSERT INTO mechanism_type_categories (name)
+VALUES 
+	("Кварцевые"),
+	("Механические"),
+	("Электронные");
+
+INSERT INTO watch_gender (name)
+VALUES 
+	("Мужские"),
+	("Женские"),
+	("Унисекс");
+
+INSERT INTO products (brand_id, mechanism_id, glass_id, gender_id, name, price)
+VALUES 
+	(3, 1, 2, 1, "Mr.Daddy", 19990),
+	(6, 1, 4, 3, "Chrono", 32250),
+	(1, 2, 4, 1, "Mega Chief", 36390),
+	(5, 1, 3, 1, "Titanium Quartz", 29090),
+	(5, 3, 2, 2, "Sport Gent", 32690),
+	(4, 3, 1, 1, "SuperSport Chrono", 40950),
+	(3, 2, 3, 2, "Powermatic", 72750),
+	(2, 1, 1, 3, "Wave", 49550),
+	(1, 2, 2, 1, "Flamingo", 23950),
+	(6, 3, 4, 3, "Heritage", 54550),
+	(3, 1, 2, 2, "Everytime Small", 23650),
+	(5, 2, 1, 1, "Bella Ora Round", 22750),
+	(4, 2, 3, 1, "Chronograph", 51850),
+	(2, 1, 3, 2, "Bridgeport", 30050),
+	(1, 1, 2, 3, "Le Locle", 59150),
+	(6, 3, 1, 1, "Carson", 49990),
+	(2, 3, 4, 2, "Ballade", 76850),
+	(5, 1, 2, 2, "Quickster", 36390),
+	(3, 2, 3, 1, "Goldrun Sapphire", 118190),
+	(4, 1, 2, 1, "Herat Flower", 63690);
---
+++ Скрипт создания таблиц.sql
@@ -0,0 +1,93 @@
+﻿DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор пользователя",
+	first_name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
+	last_name VARCHAR(100) NOT NULL COMMENT "Фамилия пользователя",
+	email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+	phone VARCHAR(50) NOT NULL UNIQUE COMMENT "Телефон",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+	) COMMENT "Пользователи";
+
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Ссылка на пользователя",
+	gender enum('M', 'F') NOT NULL COMMENT "Пол",
+	birthday DATE NOT NULL COMMENT "Дата рождения",
+	country VARCHAR(50) NOT NULL COMMENT "Страна проживания",
+	city VARCHAR(50) NOT NULL COMMENT "Город проживания",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Профили";
+
+DROP TABLE IF EXISTS brands;
+CREATE TABLE brands (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор бренда",
+	name VARCHAR(100) NOT NULL COMMENT "Название бренда",
+	country VARCHAR(100) NOT NULL COMMENT "Страна производителя"
+	) COMMENT "Бренды";
+
+DROP TABLE IF EXISTS mechanism_type_categories;
+CREATE TABLE mechanism_type_categories (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор типа",
+	name VARCHAR(100) NOT NULL COMMENT "Тип механизма"
+	) COMMENT "Типы механизма";
+
+DROP TABLE IF EXISTS glass_type_categories;
+CREATE TABLE glass_type_categories (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор типа",
+	name VARCHAR(100) NOT NULL COMMENT "Тип стекла"
+	) COMMENT "Типы стекла";
+
+DROP TABLE IF EXISTS watch_gender;
+CREATE TABLE watch_gender (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор типа",
+	name VARCHAR(50) NOT NULL COMMENT "Тип часов по половой принадлежности"
+	) COMMENT "Типы часов";
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор товара",
+	brand_id INT UNSIGNED NOT NULL COMMENT "Идентификатор бренда",
+	mechanism_id INT UNSIGNED NOT NULL COMMENT "Идентификатор бренда",
+	glass_id INT UNSIGNED NOT NULL COMMENT "Идентификатор бренда",
+	gender_id INT UNSIGNED NOT NULL COMMENT "Идентификатор бренда",
+	name VARCHAR(255) NOT NULL COMMENT "Название",
+	price DECIMAL NOT NULL COMMENT "Цена",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+	) COMMENT "Товары";
+
+DROP TABLE IF EXISTS customer_comments;
+CREATE TABLE customer_comments (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор отзыва",
+	user_id INT UNSIGNED NOT NULL COMMENT "Идентификатор пользователя, оставившего отзыв",
+	product_id INT UNSIGNED NOT NULL COMMENT "Идентификатор товара, на который оставили отзыв",
+	body TEXT NOT NULL COMMENT "Текст отзыва",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+    ) COMMENT "Отзывы";
+   
+DROP TABLE IF EXISTS users_discounts;
+CREATE TABLE users_discounts (
+	user_id INT UNSIGNED NOT NULL COMMENT "Идентификатор пользователя",
+	discount INT UNSIGNED NOT NULL COMMENT "Персональная скидка"
+	);
+
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор заказа",
+	user_id INT UNSIGNED NOT NULL COMMENT "Идентификатор пользователя, сделавшего заказ",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+	);
+
+DROP TABLE IF EXISTS orders_products;
+CREATE TABLE orders_products (
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+	order_id INT UNSIGNED NOT NULL COMMENT "Идентификатор заказа",
+	product_id INT UNSIGNED NOT NULL COMMENT "Идентификатор товара, входящего в заказ",
+	total INT UNSIGNED NOT NULL COMMENT "Количество товара в заказе",
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+	);
